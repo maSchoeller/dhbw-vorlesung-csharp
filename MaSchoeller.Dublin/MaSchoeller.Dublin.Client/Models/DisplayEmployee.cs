5f8648d10a4a2d45d9fbb0f45af733dc1df6e39b
Marvin Schoeller <microsoft@marvinschoeller.de>
2020-06-16 14:58:33 +0200

    add some boilerplate

---
+++ MaSchoeller.Dublin/MaSchoeller.Dublin.Client/Models/DisplayEmployee.cs
@@ -0,0 +1,15 @@
+﻿using MaSchoeller.Extensions.Desktop.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaSchoeller.Dublin.Client.Models
+{
+    public class DisplayEmployee : NotifyPropertyChangedBase
+    {
+       
+
+    }
+}
--- MaSchoeller.Dublin/MaSchoeller.Dublin.Client/ViewModels/ConfigBuisnessUnitViewModel.cs
+++ MaSchoeller.Dublin/MaSchoeller.Dublin.Client/ViewModels/ConfigBuisnessUnitViewModel.cs
@@ -1,13 +1,34 @@
-﻿using MaSchoeller.Extensions.Desktop.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using MaSchoeller.Dublin.Client.Models;
+using System.Windows.Input;
 
 namespace MaSchoeller.Dublin.Client.ViewModels
 {
     public class ConfigBuisnessUnitViewModel : ViewModelBase
     {
+
+        public ConfigBuisnessUnitViewModel()
+        {
+
+        }
+
+        private ObservableCollection<DisplayBuisnessUnit> _buisnessUnits
+            = new ObservableCollection<DisplayBuisnessUnit>();
+        public ObservableCollection<DisplayBuisnessUnit> BuisnessUnits
+        {
+            get => _buisnessUnits;
+            set => SetProperty(ref _buisnessUnits, value);
+        }
+
+        private DisplayBuisnessUnit _selectedBuisnessUit;
+        public DisplayBuisnessUnit SelectedBuisnessUnit
+        {
+            get => _selectedBuisnessUit;
+            set => SetProperty(ref _selectedBuisnessUit, value);
+        }
+
+        public ICommand NewCommand { get; set; } = null!;
+        public ICommand EditCommand { get; set; } = null!;
+        public ICommand SaveCommand { get; set; } = null!;
     }
 }
